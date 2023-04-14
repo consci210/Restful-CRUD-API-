bbf832301cbbe4f877579c6af7a306a6a52b4ef1
Helina Ashenafi <Consci210@gmail.com>
2023-04-14 14:40:00 +0300

    added get and post route handlers

--- server.js
+++ server.js
@@ -1,15 +1,65 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Customer = require('./models/customerModel')
 
 
 const app = express()
+app.use(express.json())
+
+   
+    
+    
 
 
-
-// routes 
+   
+// GET requests 
+// Go to homepage 
 app.get('/' , (req , res) => {
     res.send("Greetings from Node JS ...server listening on port 3000 ")
 })
+
+ // Get all customers   
+app.get('/customers' , async(req,res) =>{
+    try{
+       
+        const customers = await Customer.find({})  
+        res.status(200).json(customers)
+    }catch(err){
+        console.log(err.message)
+        res.status(500).json({message : err.message })
+    }
+})
+
+// Get a specific customer  
+
+app.get('/customers/:id' , async(req,res) => {
+    try{
+        const {id} = req.params 
+        const customer = await Customer.findById(id)
+        res.status(200).json(customer) 
+    }catch(err){
+        console.log(err.message)
+        res.status(500).json({message : err.message })
+    }
+})
+
+
+
+
+// POST request
+app.post('/customer' , async(req , res) => {
+    try{
+        const customer = await Customer.create(req.body)
+        res.status(200).json(customer)
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({message : err.message })
+    }
+    
+})
+
+
 
 mongoose.set("strictQuery" , false )
 mongoose
