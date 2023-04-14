58d14a72f5543c1d3c9318683c1985b6bf1b9095
Helina Ashenafi <Consci210@gmail.com>
2023-04-14 15:10:23 +0300

    added put and delete requests

--- server.js
+++ server.js
@@ -5,13 +5,25 @@ const Customer = require('./models/customerModel')
 
 const app = express()
 app.use(express.json())
-
+app.use(express.urlencoded({extended : false }))
    
     
+
+// Connect to Database 
+mongoose.set("strictQuery" , false )
+mongoose
+.connect('mongodb+srv://admin:admin@restapi.a5y36u3.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(() => {
+    app.listen(3000 , ()=>{
+        console.log('App is running on port 3000')
+    })
+    console.log('Connected to Mongo DB') 
+}).catch( (err) => {
+    console.log(err)
+})
     
 
 
-   
 // GET requests 
 // Go to homepage 
 app.get('/' , (req , res) => {
@@ -30,8 +42,8 @@ app.get('/customers' , async(req,res) =>{
     }
 })
 
-// Get a specific customer  
 
+// Get a specific customer  
 app.get('/customers/:id' , async(req,res) => {
     try{
         const {id} = req.params 
@@ -46,7 +58,8 @@ app.get('/customers/:id' , async(req,res) => {
 
 
 
-// POST request
+// POST request 
+// Create a customer 
 app.post('/customer' , async(req , res) => {
     try{
         const customer = await Customer.create(req.body)
@@ -61,14 +74,39 @@ app.post('/customer' , async(req , res) => {
 
 
 
-mongoose.set("strictQuery" , false )
-mongoose
-.connect('mongodb+srv://admin:admin@restapi.a5y36u3.mongodb.net/Node-API?retryWrites=true&w=majority')
-.then(() => {
-    app.listen(3000 , ()=>{
-        console.log('App is running on port 3000')
-    })
-    console.log('Connected to Mongo DB') 
-}).catch( (err) => {
-    console.log(err)
+// PUT request 
+// Edit a customer 
+app.put('/customers/:id' , async(req,res) => {
+    try{
+        const {id} = req.params 
+        const customer = await Customer.findByIdAndUpdate(id , req.body) 
+        if(!customer){
+            return res.status(404).json({message : `Couldn't update the customer with id  ${id} becuase it doesnt exist in database ` })
+        }
+        const updatedCustomer = await Customer.findById(id)  
+        res.status(200).json(updatedCustomer) 
+    }catch(err){
+        console.log(err.message)
+        res.status(500).json({message : err.message })
+    }
 })
+
+
+
+// DELETE request 
+// Remove a customer 
+app.delete('/customers/:id' , async(req,res) => {
+    try{
+        const {id} = req.params 
+        const customer = await Customer.findByIdAndDelete(id) 
+        if(!customer){
+            return res.status(404).json({message : `Couldn't delete the customer with id  ${id} becuase it doesnt exist in database ` })
+        }
+        res.status(200).json(customer) 
+    }catch(err){
+        console.log(err.message)
+        res.status(500).json({message : err.message })
+    }
+})
+
+
