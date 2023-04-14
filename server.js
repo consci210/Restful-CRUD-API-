71f6488f21b33d56a18b7b553b79377a7d850f10
Helina Ashenafi <Consci210@gmail.com>
2023-04-14 13:45:25 +0300

    connected to DB

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+
 const app = express()
 
-app.listen(3000 , ()=>{
-    console.log('App is running on port 3000')
-})
+
 
 // routes 
 app.get('/' , (req , res) => {
-    res.send("Greetings from Node API ")
+    res.send("Greetings from Node JS ...server listening on port 3000 ")
+})
+
+mongoose.set("strictQuery" , false )
+mongoose
+.connect('mongodb+srv://admin:admin@restapi.a5y36u3.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(() => {
+    app.listen(3000 , ()=>{
+        console.log('App is running on port 3000')
+    })
+    console.log('Connected to Mongo DB') 
+}).catch( (err) => {
+    console.log(err)
 })
